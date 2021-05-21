deb3c64a543b1de97653089248f9abe6ac10c0df
AmandaGorni <ap.gorni@gmail.com>
2021-05-21 20:53:33 -0300

    Desafio 05 finalizado

---
+++ index.js
@@ -0,0 +1,30 @@
+// Crie um array que receba 5 itens e exiba no console.
+
+let compras = ["arroz", "feijão","macarrão","carnes","legumes"]
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+compras.unshift("verduras")
+
+// Utilize um método para remover o último nome do array.
+
+compras.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+compras.push("suco","cereais")
+
+// Utilize um método para remover o primeiro nome do array.
+
+compras.shift()
+
+console.log(compras)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+console.log(numbers.sort())
+
+
